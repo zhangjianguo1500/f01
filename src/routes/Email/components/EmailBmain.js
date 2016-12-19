228d1760a6f7f54a3dc0b310f7be65186747587b
zhangjianguo1500 <jianguo1500@163.com>
2016-12-19 18:12:38 +0800

    Create EmailBmain.js

---
+++ src/routes/Email/components/EmailBmain.js
@@ -0,0 +1,19 @@
+import React, { Component,PropTypes } from 'react';
+import './EmailBmain.scss'
+import './E-Global.scss'
+import Select, { Option } from 'rc-select'
+import '../../../components/Select/assets/index.scss'
+import '../../../styles/font.scss'
+
+const EmailR1 = React.createClass({
+
+    render(){
+        return (
+            <div className="EmailR">
+                邮件
+            </div>
+        )
+    }
+})
+
+export default EmailR1;
