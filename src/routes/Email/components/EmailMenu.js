13ac2ef1556701f271c0026ec52ffbad0c708cf4
zhangjianguo1500 <jianguo1500@163.com>
2016-12-19 18:17:57 +0800

    Update EmailMenu.js

--- src/routes/Email/components/EmailMenu.js
+++ src/routes/Email/components/EmailMenu.js
@@ -5,8 +5,7 @@ import Select, { Option } from 'rc-select'
 import '../../../components/Select/assets/index.scss'
 import '../../../styles/font.scss'
 
-const EmailBox = React.createClass({
-
+const MenulBox = React.createClass({
     render(){
         return (
             <div className="EmailMenu">
@@ -14,19 +13,24 @@ const EmailBox = React.createClass({
                     <h2>林海<span>销售经理</span></h2>
                     <div className="EmailName">sales02@china...</div>
                 </div>
-                <ul className="Operating">
-                    <li><i className="ic-i-menu"></i>写邮件</li>
-                    <li><i className="ic-i-menu"></i>收邮件</li>
-                    <li><i className="ic-i-menu"></i>收件箱</li>
-                    <li><i className="ic-i-menu"></i>已发送</li>
-                    <li><i className="ic-i-menu"></i>已删除</li>
-                    <li><i className="ic-i-menu"></i>审核箱</li>
-                    <li><i className="ic-i-menu"></i>垃圾箱</li>
-                    <li><i className="ic-i-menu"></i>草稿箱</li>
-                </ul>
+                <div className="Operating">
+                    <li><i className="ic-i-add"></i>写邮件</li>
+                    <li><i className="ic-i-add"></i>收邮件</li>
+                    <li><i className="ic-i-add"></i>收件箱</li>
+                    <li><i className="ic-i-add"></i>已发送</li>
+                    <li><i className="ic-i-add"></i>已删除</li>
+                    <li><i className="ic-i-add"></i>审核箱</li>
+                    <li><i className="ic-i-add"></i>垃圾箱</li>
+                    <li><i className="ic-i-add"></i>草稿箱</li>
+                </div>
+                <div className="Operating">
+                    <li><i className="ic-i-add"></i>我的分类</li>
+                    <li><i className="ic-i-add"></i>邮箱审核</li>
+                    <li><i className="ic-i-add"></i>共享记录</li>
+                </div>
             </div>
         )
     }
 })
 
-export default EmailBox;
+export default MenulBox;
